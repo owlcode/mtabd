70b6f7c6ca625060105544dc9cd29ceb36a96287
mariusz <mariuszer@gmail.com>
2017-10-12 21:11:19 +0200

    Initial commit of Spring Boot project

---
+++ mtabd-java/src/main/java/pl/dawidsowa/mtabd/Application.java
@@ -0,0 +1,11 @@
+package pl.dawidsowa.mtabd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
