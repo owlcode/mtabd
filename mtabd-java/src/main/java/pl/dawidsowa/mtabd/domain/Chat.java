98c4a3df42e4eef34b70937e82e30e35c8612f24
mariusz <mariuszer@gmail.com>
2017-10-12 22:21:09 +0200

    Added Chat model

---
+++ mtabd-java/src/main/java/pl/dawidsowa/mtabd/domain/Chat.java
@@ -0,0 +1,17 @@
+package pl.dawidsowa.mtabd.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hibernate_sequence")
+    @SequenceGenerator(name = "hibernate_sequence", sequenceName = "hibernate_sequence")
+    private Long id;
+
+    @OneToOne
+    private Service service;
+}
