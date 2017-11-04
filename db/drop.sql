7f632998357070da48a974b3a32265a9625a5cbe
Piotr Skrobich <pskrobich@gmail.com>
2017-11-04 13:08:39 +0100

    file with drop procedures added

---
+++ db/drop.sql
@@ -0,0 +1,11 @@
+DROP TABLE DictionaryValues;
+DROP TABLE Translations;
+DROP TABLE Dictionaries;
+DROP TABLE Messages;
+DROP TABLE Chat;
+DROP TABLE ProductsInServices;
+DROP TABLE Products;
+DROP TABLE Services;
+DROP TABLE Performers;
+DROP TABLE Users;
+GO
